311e3539c62c57b4a83e55ff07708f9eea43ef91
saravana02tech <saravanasurya7931@gmail.com>
2021-07-02 10:26:36 +0530

    new

---
+++ Name.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Name
+{
+  public static void main(String args[])
+  {
+     int num;
+ 
+     Scanner in = new Scanner(System.in);
+ 
+     //Get input String
+     System.out.println("Enter a string: ");
+     str = in.nextLine();
+     System.out.println("Input String is: "+str);
+	 }
+	 }
